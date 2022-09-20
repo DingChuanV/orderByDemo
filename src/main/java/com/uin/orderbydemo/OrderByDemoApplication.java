8232417d0221e51aec19d093112728445830f4ef
wanglufei <2686221966@qq.com>
2022-09-20 21:09:15 +0800

    初始化工程，需求分析

---
+++ src/main/java/com/uin/orderbydemo/OrderByDemoApplication.java
@@ -0,0 +1,13 @@
+package com.uin.orderbydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderByDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderByDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uin/orderbydemo/OrderByDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uin.orderbydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderByDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
